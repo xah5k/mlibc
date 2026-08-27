2a0aed5661abad34dd12465c8f0fefeb774e39c8
ah5k <ah5k_alt02@proton.me>
2026-08-27 20:03:13 +0100

    .

---
+++ sysdeps/ah5kos/generic/ah5kos_spec.cpp
@@ -0,0 +1,9 @@
+#include "/home/ah5k/osdev/base/arch/x86_64/syscallidx.h"
+#include <stdint.h>
+#include "/home/ah5k/osdev/base/kstatus.h"
+#include "../include/ah5kos.h"
+extern "C" uint64_t dosyscall(uint64_t sys_num, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,  uint64_t arg5);
+
+extern "C" {
+    
+}
---
+++ sysdeps/ah5kos/include/ah5kos.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+#include <stdint.h>
+
+// used in kernel internally to represent a point
+typedef struct {
+    uint64_t x;
+    uint64_t y;
+} Point;
+
+// mouse
+typedef struct {
+    Point RawPos;
+    int LeftClickPress;
+    int RightClickPress;
+    int MiddleClickPress;
+} KeDevMousePacket;
+
+// reading the rtc
+typedef struct {
+    uint8_t Seconds;
+    uint8_t Minutes;
+    uint8_t Hours;
+    uint8_t Days;
+    uint8_t Month;
+    uint8_t Year; // short hand as in 0-99
+} KeDevClockWallTime;
+
+typedef struct {
+    uint64_t rsv; // userspace wont be able to write to it anyway
+    uint32_t size;
+    uint32_t width;
+    uint32_t height;
+    uint32_t scanline;
+    uint64_t bpp;
+} Framebuffer;
+
+// wrapper
+typedef struct {
+    Framebuffer* fb;
+} KeDevFbInfo;
+
+#ifdef __cplusplus 
+}
+#endif
